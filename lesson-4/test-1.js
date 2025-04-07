972299013932dceea2d0dacd8b6e7db1ee8d89db
Nguyen Dang Khoa <wentallout@gmail.com>
2025-04-07 20:44:45 +0700

    feat: add test 1 answers

---
+++ lesson-4/test-1.js
@@ -0,0 +1,56 @@
+const departurePlanet = 'Trái Đất';
+const mission = 'Khám phá vũ trụ K13';
+
+const crew = [
+    'Khoa Nguyễn',
+    'Nguyên Hồ Phúc',
+    'Trang Luong',
+    'Như Trần Thị Quỳnh',
+    'Thơm Trần',
+    'Hien Trinh',
+    'Phuong Vo',
+];
+
+const code = 'K13 Challenge';
+
+function formCrew(crew) {
+    return crew.join(', ');
+}
+
+function launchShip(crew) {
+    const message = `Chuẩn bị khởi động! Phi hành đoàn gồm ${formCrew(
+        crew
+    )} sẽ đồng hành cùng bạn trong chuyến phiêu lưu ${mission}`;
+    return message;
+}
+
+function calculateDistanceInKilometers(speed, time) {
+    return speed * time;
+}
+
+function convertTimeToHex(time) {
+    return time.toString(16).toUpperCase();
+}
+
+function decryptCode(code) {
+    const arrayFromString = code.split('');
+    const transformedArray = arrayFromString.map((value) => {
+        if (value === value.toUpperCase()) {
+            return value.toLowerCase();
+        } else {
+            return value.toUpperCase();
+        }
+    });
+    return transformedArray.join('');
+}
+
+function returnToEarth() {
+    console.log('Chuẩn bị trở về Trái Đất!');
+}
+
+// Test all functions
+console.log(launchShip(crew));
+console.log(calculateDistanceInKilometers(1000, 24));
+console.log(convertTimeToHex(120));
+console.log(decryptCode(code));
+returnToEarth();
