91b264c6740402f2c385731fb3f0672f093c6e2b
Nguyen Dang Khoa <wentallout@gmail.com>
2025-04-21 16:11:47 +0700

    chore: format lesson 4 reduce

--- lesson-4/utils-function/01-array.reduce(callback).js
+++ lesson-4/utils-function/01-array.reduce(callback).js
@@ -4,23 +4,23 @@ const scores = [85, 90, 78];
 const numbers = [1, 2, 3, 4];
 const expenses = [50, 100, 150];
 
-// 6.1 Tính tổng các giá trị trong scores.
 function getSumOfScores(arr) {
     return arr.reduce((acc, value) => acc + value, 0);
 }
 
-console.log(getSumOfScores(scores));
-
-// 6.2 Tính tích các giá trị trong numbers.
 function productOfNumbers(arr) {
     return arr.reduce((acc, value) => acc * value, 1); //the 1 is actually important cuz you dont want to multiply zero
 }
 
-console.log(productOfNumbers(numbers));
-
-// 6.3 Tính tổng các giá trị trong expenses.
 function getSumOfExpenses(arr) {
     return arr.reduce((acc, value) => acc + value, 0);
 }
 
+// 6.1 Tính tổng các giá trị trong scores.
+console.log(getSumOfScores(scores));
+
+// 6.2 Tính tích các giá trị trong numbers.
+console.log(productOfNumbers(numbers));
+
+// 6.3 Tính tổng các giá trị trong expenses.
 console.log(getSumOfExpenses(expenses));
